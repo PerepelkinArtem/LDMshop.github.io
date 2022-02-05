549038c4480c61a8cc44a08c323b411b008a22a4
PerepelkinArtem <perepelkin-klg@yandex.ru>
2022-02-05 21:12:28 +0300

    Creation first page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,18 @@
 function Card() {
-    return (
-        <div className="card">
-        <img src="/img/auto/1.jpg" alt="auto" />
-        <p>VW JETTA 6 2014 1.4 CAXC DQ200 DSG7</p>
-        <div>
-          <span>Статус: </span>
-          <b>В разборе</b>
-          <button className="button">
-            <img width={15} height={15} src="/img/plus.svg" alt="plus"/>
-          </button>
-        </div>
+  return (
+    <div className="card">
+      <img src="/img/auto/1.jpg" alt="auto" />
+      <p>VW JETTA 6 2014 1.4 DSG7</p>
+      <div className="card-status">
+        <span>Статус:</span>
+        <b>В разборе</b>
+        <img src="/img/plus.svg" alt="plus" />
+        {/* <button className="button">
+          <img src="/img/plus.svg" alt="plus" />
+        </button> */}
       </div>
-    );
+    </div>
+  );
 }
 
 export default Card
