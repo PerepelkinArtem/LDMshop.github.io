db99a4d9244fd19437f7538665aa4eabecf2c09c
PerepelkinArtem <perepelkin-klg@yandex.ru>
2022-02-18 16:48:36 +0300

    new Card

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Card from './components/Card';
 import Header from './components/Header';
 
 const arr = [
+  { title: 'SKODA OCTAVIA A5 2011 1,6 BSE MT', price: 40000, imageUrl: '/img/auto/5.jpg' },
+  { title: 'VW JETTA 6 2011 1.4 DSG', price: 20000, imageUrl: '/img/auto/2.jpg' },
+  { title: 'VW POLO 1.6MPI AT', price: 5000, imageUrl: '/img/auto/3.jpg' },
+  { title: 'VW PASSAT B6 2008 1,8 BZB AT', price: 50000, imageUrl: '/img/auto/4.jpg' },
   { title: 'VW JETTA 6 2014 1.4 DSG7', price: 10000, imageUrl: '/img/auto/1.jpg' },
-  { title: 'SKODA Octavia 1.6MPI MT ', price: 20000, imageUrl: '/img/auto/1.jpg' },
-  { title: 'SKODA Rapid 1.6MPI MT ', price: 5000, imageUrl: '/img/auto/1.jpg' },
-  { title: 'SKODA Karoq 1.6MPI MT ', price: 50000, imageUrl: '/img/auto/1.jpg' },
 ];
 
 function App() {
